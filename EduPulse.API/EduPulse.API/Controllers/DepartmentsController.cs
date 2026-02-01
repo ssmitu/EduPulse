d4836b1facce8b6f6a20b3f5eb8f7aec08b5d32b
ssmitu <siddikasultanamitu.00@gmail.com>
2026-02-01 15:00:56 +0600

    deptcontrollersignup

---
+++ EduPulse.API/EduPulse.API/Controllers/DepartmentsController.cs
@@ -0,0 +1,23 @@
+﻿using EduPulse.API.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace EduPulse.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DepartmentsController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+        public DepartmentsController(ApplicationDbContext context) => _context = context;
+
+        [HttpGet]
+        public async Task<IActionResult> GetDepartments()
+        {
+            var depts = await _context.Departments
+                .Select(d => new { d.Id, d.Name })
+                .ToListAsync();
+            return Ok(depts);
+        }
+    }
+}
