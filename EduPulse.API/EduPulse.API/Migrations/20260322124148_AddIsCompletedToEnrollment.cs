66ce28b0599a05e4d436825f3a056253cd0bd212
ssmitu <siddikasultanamitu.00@gmail.com>
2026-03-22 19:41:02 +0600

    batch course update- student side

---
+++ EduPulse.API/EduPulse.API/Migrations/20260322124148_AddIsCompletedToEnrollment.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EduPulse.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddIsCompletedToEnrollment : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsCompleted",
+                table: "Enrollments",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsCompleted",
+                table: "Enrollments");
+        }
+    }
+}
